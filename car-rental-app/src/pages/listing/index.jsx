e79da1af2390c68fb1d2ea145607374320168d8e
Abhishek A S <abhishekas2023a@mca.ajce.in>
2025-07-09 16:09:31 +0530

    setting layouts

--- car-rental-app/src/pages/listing/index.jsx
+++ car-rental-app/src/pages/listing/index.jsx
@@ -13,7 +13,16 @@ function index() {
   }, []);
 
   return (
-    <div>{JSON.stringify(listings)}</div>
+    // <div>{JSON.stringify(listings)}</div>
+    <>
+<div className="w-[1400px] mx-auto">  <h3 className='text-2xl font-bold mt-4 mb-4'>Listings</h3>
+    <div>
+      <div className=' border-2 '>
+        ss
+      </div>
+    </div>
+  </div>
+    </>
   )
 }
 
